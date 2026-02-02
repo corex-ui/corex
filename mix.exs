53fd6035bda79f1b149bc18c5b42f7355bddbcea
karimsemmoud <karim.semmoud@netoum.com>
2026-02-02 13:19:58 +0700

    up version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Corex.MixProject do
   def project do
     [
       app: :corex,
-      version: "0.1.0-alpha.8",
+      version: "0.1.0-alpha.9",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
