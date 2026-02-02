96f106e8724faf6fa03768326d8a51f8f4d1ab2f
karimsemmoud <karim.semmoud@netoum.com>
2026-02-03 01:48:55 +0700

    core(deps): hex and package version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Corex.MixProject do
   def project do
     [
       app: :corex,
-      version: "0.1.0-alpha.9",
+      version: "0.1.0-alpha.10",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
@@ -86,7 +86,8 @@ defmodule Corex.MixProject do
         Corex.ToggleGroup
       ],
       Helpers: [
-        Corex.Collection.Item
+        Corex.Collection.Item,
+        Corex.Accordion.Item
       ]
     ]
   end
