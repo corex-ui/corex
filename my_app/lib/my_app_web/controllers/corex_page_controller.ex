db30063c5fd03ca339ceac750a10922cdb02d0db
karimsemmoud <karim.semmoud@netoum.com>
2026-04-27 01:01:43 +0700

    remove my_app

--- my_app/lib/my_app_web/controllers/corex_page_controller.ex
+++
@@ -1,7 +0,0 @@
-defmodule MyAppWeb.CorexPageController do
-  use MyAppWeb, :controller
-
-  def index(conn, _params) do
-    render(conn, :index, layout: {MyAppWeb.Layouts, :corex})
-  end
-end
--- my_app/lib/my_app_web/controllers/corex_page_html.ex
+++
@@ -1,5 +0,0 @@
-defmodule MyAppWeb.CorexPageHTML do
-  use MyAppWeb, :html
-
-  embed_templates "corex_page/*"
-end
