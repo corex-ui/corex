ed9b10801a6c2a44deb1eb1e8975fc2a071743eb
karimsemmoud <karim.semmoud@netoum.com>
2026-04-27 01:55:32 +0700

    e2e deps

--- e2e/mix.exs
+++ e2e/mix.exs
@@ -83,7 +83,8 @@ defmodule E2e.MixProject do
       {:tidewave, "~> 0.5.5", only: :dev},
       {:designex, "~> 1.0"},
       {:usage_rules, "~> 1.2", only: [:dev, :test]},
-      {:igniter, "~> 0.6", only: [:dev, :test]}
+      {:igniter, "~> 0.6", only: [:dev, :test]},
+      {:json_polyfill, "~> 0.2"}
     ]
   end
 
