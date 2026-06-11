26ed919a8f79412ee682215cf3d3ce3cbbd774d4
karimsemmoud <karim.semmoud@netoum.com>
2026-06-11 09:31:42 +0700

    Widen phoenix_live_view constraint to ~> 1.1

--- e2e/mix.exs
+++ e2e/mix.exs
@@ -61,7 +61,7 @@ defmodule E2e.MixProject do
       {:phoenix_html, "~> 4.1"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_dashboard, "~> 0.8.3"},
-      {:phoenix_live_view, "~> 1.1.0"},
+      {:phoenix_live_view, "~> 1.1"},
       {:live_capture, "~> 0.2"},
       {:lazy_html, ">= 0.1.0", only: :test},
       {:esbuild, "~> 0.10", runtime: Mix.env() == :dev},
--- integration_test/mix.exs
+++ integration_test/mix.exs
@@ -44,7 +44,7 @@ defmodule Corex.Integration.MixProject do
       {:postgrex, ">= 0.0.0"},
       {:ecto_sqlite3, ">= 0.0.0"},
       {:phoenix_html, "~> 4.1"},
-      {:phoenix_live_view, "~> 1.1.0"},
+      {:phoenix_live_view, "~> 1.1"},
       {:dns_cluster, "~> 0.2.0"},
       {:lazy_html, ">= 0.1.0"},
       {:phoenix_live_reload, "~> 1.2"},
